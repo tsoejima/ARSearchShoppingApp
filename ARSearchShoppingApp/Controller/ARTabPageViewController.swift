b608a220e27f14f2bc9ccf9448f769767b788b8c
tsoejima <t.soejimadeveloper@gmail.com>
2020-10-22 17:02:09 +0900

    Add

---
+++ ARSearchShoppingApp/Controller/ARTabPageViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ARTabPageViewController.swift
+//  ARSearchShoppingApp
+//
+//  Created by 副島拓哉 on 2020/10/22.
+//
+
+import UIKit
+
+class ARTabPageViewController: UIViewController, UINavigationControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
